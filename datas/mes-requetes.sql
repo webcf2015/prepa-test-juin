97ca9c7f32cbe5013d3223337a62516009d5a4a4
michaelp <michael.pitz@cf2m.be>
2015-06-19 09:54:07 +0200

    insertion db

--- datas/mes-requetes.sql
+++ datas/mes-requetes.sql
@@ -4,3 +4,7 @@ SELECT  u.id, u.lemail, u.lenom,
 	FROM utilisateur u
 		INNER JOIN droit d ON u.droit_id = d.id
     WHERE u.lelogin='Admin' AND u.lemdp = 'admin';
+
+/* Requete d'insertion d'une image dans la db */
+INSERT INTO photo (lenom,letype,lepoids,lahauteur,lalargeur,letitre,ladesc,utilisateur_id) 
+	VALUES ('h4qoqkffb3r0god3o71fqaqj1','jpg',762000,768,1024,'Koala','un beau koala',1);
